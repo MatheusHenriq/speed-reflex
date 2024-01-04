e8dd2d083f474653e9f92d6b373bde3dac37ba2a
Matheus Henrique <matheus@obi.tec.br>
2024-01-04 15:23:45 -0300

    feat: confetti animation

--- lib/modules/game/view/game_view.dart
+++ lib/modules/game/view/game_view.dart
@@ -1,4 +1,5 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -8,10 +9,23 @@ import '../../../core/constants/app_images.dart';
 import '../../../core/constants/app_sounds.dart';
 import '../../../widgets/card_container.dart';
 
-class GameView extends StatelessWidget {
+class GameView extends StatefulWidget {
   final GameController controller;
   const GameView({super.key, required this.controller});
   static const route = "/game/";
+
+  @override
+  State<GameView> createState() => _GameViewState();
+}
+
+class _GameViewState extends State<GameView> {
+  final ConfettiController confettiController = ConfettiController();
+  @override
+  void dispose() {
+    confettiController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(
     BuildContext context,
@@ -44,18 +58,26 @@ class GameView extends StatelessWidget {
               ),
             ),
             trailing: Text(
-              "Level ${ref.watch(controller.levelProvider)}",
+              "Level ${ref.watch(widget.controller.levelProvider)}",
               style: const TextStyle(color: Colors.white, fontSize: 14),
             ),
           );
         }),
       ),
       body: Stack(
+        alignment: Alignment.topCenter,
         children: [
           Image.asset(
             AppImages.landingBackgroundImageJPEG,
             fit: BoxFit.cover,
             width: MediaQuery.of(context).size.width,
+          ),
+          ConfettiWidget(
+            blastDirectionality: BlastDirectionality.explosive,
+            confettiController: confettiController,
+            numberOfParticles: 7,
+            emissionFrequency: 0.2,
+            gravity: 0.3,
           ),
           Row(
             children: [
@@ -76,21 +98,21 @@ class GameView extends StatelessWidget {
                       return CardContainer(
                         onTap: (newCardData) {
                           if (newCardData.isSelected == false) {
-                            ref.read(controller.levelProvider.notifier).state = 0;
-                            controller.createNewGame(ref: ref);
+                            ref.read(widget.controller.levelProvider.notifier).state = 0;
+                            widget.controller.createNewGame(ref: ref);
                             return;
                           }
-                          ref.read(controller.cardListProvider.notifier).updateCard(
-                                cardList: ref.watch(controller.cardListProvider),
+                          ref.read(widget.controller.cardListProvider.notifier).updateCard(
+                                cardList: ref.watch(widget.controller.cardListProvider),
                                 cardData: newCardData,
                                 index: index,
                               );
                         },
-                        cardData: ref.watch(controller.cardListProvider)[index],
+                        cardData: ref.watch(widget.controller.cardListProvider)[index],
                       );
                     }),
                   ),
-                  itemCount: controller.maxCards,
+                  itemCount: widget.controller.maxCards,
                 ),
               ),
               Flexible(
@@ -102,16 +124,22 @@ class GameView extends StatelessWidget {
                       padding: const EdgeInsets.only(left: 6, bottom: 32),
                       child: Consumer(builder: (context, ref, child) {
                         return MaterialButton(
-                          color: controller.allSelectableCardsSelected(ref: ref) ? Colors.green[700] : Colors.grey,
+                          color: widget.controller.allSelectableCardsSelected(ref: ref) ? Colors.green[700] : Colors.grey,
                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
                           onPressed: () async {
-                            if (controller.allSelectableCardsSelected(ref: ref)) {
+                            if (widget.controller.allSelectableCardsSelected(ref: ref)) {
                               AudioPlayer audioPlayer = AudioPlayer();
                               await audioPlayer.play(
                                 AssetSource(AppSounds.nextLevetClickSound),
                               );
-                              ref.read(controller.levelProvider.notifier).state++;
-                              controller.createNewGame(ref: ref);
+                              ref.read(widget.controller.levelProvider.notifier).state++;
+                              if (ref.watch(widget.controller.levelProvider) % 10 == 0) {
+                                confettiController.play();
+                              }
+                              Future.delayed(const Duration(milliseconds: 1500), () {
+                                confettiController.stop();
+                              });
+                              widget.controller.createNewGame(ref: ref);
                             }
                           },
                           child: const Text(
